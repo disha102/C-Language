175767e7d2a58baafc59a9a8c2f6d1ba43aebfba
disha102 <disha2006shah@gmail.com>
2025-07-22 22:01:38 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,6 @@
-# include <stdio.h>
-#include <string.h> 
-int main() 
+#include <stdio.h>
+int main()
 {
-  char str1[50]="hello";
-  char str2[50]="world";
-  strcat(str1,str2);
-  printf("concatination of a string: %s",str1);
-    return 0;
+  printf("HELLO PRINT");
+return 0;
 }
